565bb4d17a91e34017a0051c5ddaede864ddee52
Daniel Maslowski <info@orangecms.org>
2021-04-06 01:31:03 +0200

    add script for building u-root rootfs as squashfs

---
+++ build-arm-sfs.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+export GO111MODULE=off
+export GOARCH=arm
+_NAME=u-root-victure
+_DIR=/tmp/$_NAME
+
+u-root -o $_CPIO \
+  -files cpu_rsa.pub:key.pub \
+  -files usr/modules/8188fu.ko:lib/modules/8188fu.ko \
+  -files usr/modules/otg-hs.ko:lib/modules/otg-hs.ko \
+  -files usr/modules/sdio_wifi.ko:lib/modules/sdio_wifi.ko \
+  core \
+  github.com/u-root/cpu/cmds/cpud
+
+sudo rm -rf $_DIR
+mkdir -p $_DIR
+(cd $_DIR && sudo cpio -i < $_NAME.cpio)
+mksquashfs $_DIR $_NAME.sfs -comp xz
