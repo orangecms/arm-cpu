54faa26d3460e14e7feb39b517ef2157b1e80442
Daniel Maslowski <info@orangecms.org>
2021-05-09 20:55:38 +0200

    HI3536DV100/build-uimage.sh: fix paths to patches

--- hisilicon/HI3536DV100/build-uimage.sh
+++ hisilicon/HI3536DV100/build-uimage.sh
@@ -3,7 +3,7 @@
 wget https://mirrors.edge.kernel.org/pub/linux/kernel/v4.x/linux-4.9.37.tar.xz
 tar -xf linux-4.9.37.tar.xz
 cd linux-4.9.37
-patch -p1 < hi3536dv100_for_linux-4.9.y.patch
-patch -p1 < ../11_fix_yylloc_for_modern_computers.patch
+patch -p1 < ../hi3536dv100_for_linux-4.9.y.patch
+patch -p1 < ../../11_fix_yylloc_for_modern_computers.patch
 cp /tmp/u-root-arm.cpio .
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage -j9
